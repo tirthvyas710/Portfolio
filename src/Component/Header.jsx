5fa3f627ebe3600771fbee5fea64f2f063d3af61
Tirth Vyas <107625502+tirthvyas710@users.noreply.github.com>
2024-12-03 00:16:04 +0530

    Add files via upload

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -4,7 +4,7 @@ import 'remixicon/fonts/remixicon.css'
 const Header = () => {
   return (
     <div className='fixed  top-10 right-[10%] flex justify-end items-center z-50'>
-        <button className=' text-white text-xl px-4 py-2 rounded-full border-4 border-white bg-black  hover:bg-gray-400'>Hire Me</button>
+        <button className=' text-white text-xl px-4 py-2 rounded-full border-4 border-white bg-black  hover:bg-[#707070]'>Hire Me</button>
        
         
         <i className="ri-disc-fill text-[#c3c3c3] ml-10 text-5xl hover:rotate-[720deg] transition-all duration-1000"></i>
